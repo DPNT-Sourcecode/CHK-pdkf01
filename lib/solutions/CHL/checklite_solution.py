72db94c5efb0f2136499bfc48b1dae5f0fdb8ce3
sbx_user1051 <sbx_user1051@169.254.168.21>
2022-06-13 15:25:55 +0000

    Mon Jun 13 15:25:55 UTC 2022

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -3,8 +3,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def totalValue(basket):
-
+    total = 0
+    for i in range(len(basket)):
+        if basket[]
 
 def checklite(skus):
     if skus == "A"
+
 
