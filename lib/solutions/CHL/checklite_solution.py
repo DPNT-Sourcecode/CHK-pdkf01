b03bd7e8092cc3ce0267935aab8b4b20062f7571
sbx_user1051 <sbx_user1051@169.254.168.21>
2022-06-13 15:28:54 +0000

    Mon Jun 13 15:28:54 UTC 2022

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -5,9 +5,19 @@
 def totalValue(basket):
     total = 0
     for i in range(len(basket)):
-        if basket[]
+        if basket[i] == "A" or "B" or "C" or "D":
+            if basket[i] =="A":
+                total+=50
+            if basket[i] =="B":
+                total+=50
+            if basket[i] =="C":
+                total+=50
+            if basket[i] =="D":
+                total+=50
+
 
 def checklite(skus):
     if skus == "A"
+
 
 
