281765612a2954b93c0a6ac49de3c86fb00a940b
sbx_user1051 <sbx_user1051@169.254.168.21>
2022-06-13 14:55:34 +0000

    Mon Jun 13 14:55:34 UTC 2022

--- test/solution_tests/HELLO/test_hello.py
+++ test/solution_tests/HELLO/test_hello.py
@@ -3,7 +3,8 @@ from solutions.HLO import hello_solution
 
 class TestSum():
     def test_sum(self):
-        assert hello_solution.hello("Hello World") == "Hello, World"
-        assert hello_solution.hello("") == "Hello, World"
-        assert hello_solution.hello("Hi") == "Hello, World"
+        assert hello_solution.hello("Hello World") == "Hello, World!"
+        assert hello_solution.hello("") == "Hello, World!"
+        assert hello_solution.hello("Hi") == "Hello, World!"
+
 
