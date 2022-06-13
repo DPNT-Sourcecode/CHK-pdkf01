9c6d63671e66cc4c86d49b36c6d1766abba51e2c
sbx_user1051 <sbx_user1051@169.254.168.21>
2022-06-13 15:37:54 +0000

    Mon Jun 13 15:37:54 UTC 2022

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -4,23 +4,25 @@
 # skus = unicode string
 def totalValue(basket):
     total = 0
+    orderDict = {"A":0, }
     for i in range(len(basket)):
-        if basket[i] == "A" or "B" or "C" or "D":
-            if basket[i] =="A":
-                total+=50
-            if basket[i] =="B":
-                total+=30
-            if basket[i] =="C":
-                total+=20
-            if basket[i] =="D":
-                total+=15
-        elif basket[i] == "3" or "5":
-            if basket[i] == 3
+        # if basket[i] == "A" or "B" or "C" or "D":
+        #     if basket[i] =="A":
+        #         total+=50
+        #     if basket[i] =="B":
+        #         total+=30
+        #     if basket[i] =="C":
+        #         total+=20
+        #     if basket[i] =="D":
+        #         total+=15
+        # elif basket[i] == "3" or "5":
+        #     if basket[i] == 3
 
 
 
 def checklite(skus):
     if skus == "A"
+
 
 
 
