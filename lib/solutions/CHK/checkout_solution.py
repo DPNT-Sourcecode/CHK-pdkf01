060aabb7d57c7915639362d17176077ccb28978f
sbx_user1051 <sbx_user1051@169.254.220.245>
2022-06-15 06:42:59 +0000

    Wed Jun 15 06:42:59 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,9 +32,10 @@ def totalValue(basket):
     for alphabet in alphabets:
         orderDict[alphabet] = 0
     # orderDict = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0}
-    itemPriceDict = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0,
-                     'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0,
-                     'V': 0, 'W': 0, 'X': 90, 'Y': 10, 'Z': 50}
+    itemPriceDict = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10, 'G': 20, 'H': 10, 'I': 35, 'J': 60,
+                     'K': 80, 'L': 90,
+                     'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50, 'S': 30, 'T': 20, 'U': 40,
+                     'V': 50, 'W': 20, 'X': 90, 'Y': 10, 'Z': 50}
     for i in range(len(basket)):
         product = str(basket[i])
         orderDict[product] = orderDict.get(product) + 1
@@ -54,6 +55,13 @@ def totalValue(basket):
             total = total + orderDict[item] * 20
         if item == "D":
             total = total + orderDict[item] * 15
+
+        if item == "A":
+            if orderDict[item] < 3:
+                total = total + orderDict[item] * 50
+            elif orderDict[item] >= 3:
+                total = recursionForA(orderDict[item], total)
+
         if item == "A":
             if orderDict[item] < 3:
                 total = total + orderDict[item] * 50
@@ -97,3 +105,4 @@ def checkout(skus):
         finalPrice = totalValue(skus)
 
     return finalPrice
+
